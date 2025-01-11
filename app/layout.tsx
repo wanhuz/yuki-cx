42116df144bb5226841e30575bf60d8cf3feb3a0
wan <wanhuz96@gmail.com>
2025-01-11 09:57:25 +0800

    Fix favicon not displayed

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,6 +28,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+      <link rel="icon" href="/images/favicon.ico" sizes="any" />
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <Header></Header>
         {children}
