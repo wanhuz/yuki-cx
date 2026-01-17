12aef468f32450c7dc3c89c7019fb29e42e44967
wan <wanhuz96@gmail.com>
2026-01-17 12:36:24 +0800

    Update image configuration option

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,27 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      domains: ['mei.kuudere.pw', 'artworks.thetvdb.com'], // Add the external domain here
-    },
-    async redirects() {
-      return [
-        {
-          // this will match `/english(default)/something` being requested
-          source: '/',
-          destination: '/search',
-          permanent: false
-        },
-      ]
-    },
-  };
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'mei.kuudere.pw',
+      },
+      {
+        protocol: 'https',
+        hostname: 'artworks.thetvdb.com',
+      },
+    ],
+  },
 
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/search',
+        permanent: false,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
